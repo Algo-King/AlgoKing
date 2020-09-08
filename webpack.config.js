66c7d3b22776ec54c41a8aa6a6ba7a02fd626512
Frank Norton <jfnorton32@gmail.com>
2020-09-08 18:59:13 -0400

    further boilerplate additions

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: "babel-loader",
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/transform-runtime']
+        }
       },
       {
         test: /\.css$/,
