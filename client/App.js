0064e61574cacf59ba4a0b62cdd961f4305dabde
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-08 14:32:33 -0700

    frontend router setup

--- client/App.js
+++ client/App.js
@@ -1,7 +1,16 @@
-import React from "react";
-class App extends React.Component {
-  render() {
-    return <div>Totally awesome app.</div>;
-  }
-}
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./layout/Home";
+import Leaderboard from "./layout/Leaderboard";
+import Login from "./layout/Login";
+import Register from "./layout/Register";
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/register" component={Home} />
+    </Fragment>
+  </Router>
+);
 export default App;
---
+++ client/layout/Leaderboard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Leaderboard = () => {
+  return <div>Leaderboard</div>;
+};
