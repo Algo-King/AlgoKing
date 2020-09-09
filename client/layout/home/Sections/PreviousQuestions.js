ffb7a5474966c4995df399a24eed600012d5015e
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-09 14:01:04 -0700

    component cleanup

--- client/layout/home/Sections/PreviousQuestions.js
+++ client/layout/home/Sections/PreviousQuestions.js
@@ -1,29 +1,13 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-// @material-ui/icons
-import TrendingUp from "@material-ui/icons/TrendingUp";
 // core components
 import GridContainer from "../../material-ui/GridContainer.js";
 import GridItem from "../../material-ui/GridItem.js";
 import Card from "../../material-ui/Card.js";
-import CardBody from "../../material-ui/CardBody.js";
-import CardHeader from "../../material-ui/CardHeader.js";
 import Info from "../../material-ui/Info.js";
-import Danger from "../../material-ui/Danger.js";
-import Success from "../../material-ui/Success.js";
-import Button from "../../material-ui/Button.js";
 
 import blogsStyle from "../../../assets/jss/material-kit-pro-react/views/sectionsSections/blogsStyle";
-
-import cardBlog4 from "../../../assets/img/examples/card-blog4.jpg";
-import office2 from "../../../assets/img/office2.jpg";
-import blog5 from "../../../assets/img/examples/blog5.jpg";
-import blog6 from "../../../assets/img/examples/blog6.jpg";
-import blog7 from "../../../assets/img/examples/blog7.jpg";
-import blog8 from "../../../assets/img/examples/blog8.jpg";
-import bg5 from "../../../assets/img/bg5.jpg";
-
 const useStyles = makeStyles(blogsStyle);
 
 export default function SectionBlogs({ ...rest }) {
@@ -41,7 +25,6 @@ export default function SectionBlogs({ ...rest }) {
               className={classes.mlAuto + " " + classes.mrAuto}
             >
               <h2 className={classes.title}>Previous Questions</h2>
-              <br />
               <Card plain blog className={classes.card}>
                 <GridContainer>
                   <GridItem xs={12} sm={12} md={12}>
--- client/layout/home/Sections/SectionHeaders.js
+++ client/layout/home/Sections/SectionHeaders.js
@@ -8,42 +8,22 @@ import Carousel from "react-slick";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-// @material-ui/icons
-import Share from "@material-ui/icons/Share";
-import ShoppingCart from "@material-ui/icons/ShoppingCart";
 // core components -- comes from material-ui folder
 import Header from "../../material-ui/Header";
 import Button from "../../material-ui/Button.js";
 import GridContainer from "../../material-ui/GridContainer.js";
 import GridItem from "../../material-ui/GridItem.js";
-import Card from "../../material-ui/Card.js";
-import CardBody from "../../material-ui/CardBody.js";
-import CustomInput from "../../material-ui/CustomInput.js";
 
 import headersStyle from "../../../assets/jss/material-kit-pro-react/views/sectionsSections/headersStyle";
-
-import bg12 from "../../../assets/img/bg12.jpg";
 import office2 from "../../../assets/img/examples/office2.jpg";
-import dg1 from "../../../assets/img/dg1.jpg";
-import dg2 from "../../../assets/img/dg2.jpg";
-import dg3 from "../../../assets/img/dg3.jpg";
 
 const useStyles = makeStyles(headersStyle);
 
 export default function SectionHeaders({ ...rest }) {
   const classes = useStyles();
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-  };
   return (
     // we've set the className to cd-section so we can make smooth scroll to it
     <div className="cd-section" {...rest}>
-      {/* <div className={classes.sectionBlank} id="blanksection" /> */}
       {/* HEADER 2 START */}
       <div>
         <Header
