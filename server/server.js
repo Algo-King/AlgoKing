a4fff2e8a3e3776cf5d7ef45924066522abe6101
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-09-08 18:55:27 -0700

    server setup

--- server/server.js
+++ server/server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const path = require('path');
-const signup = require('./routers/signupRoute');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 5000;
+
 // parse request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.resolve(__dirname, "client")));
 
-app.use('/signup', signup);
-
+// Error Handlers
 app.use((err, req, res, next) => {
-	const defaultErr = {
-		log: 'Express error handler caught unknown middleware error',
-		status: 400,
-		message: { err: 'An error occurred' },
-	};
-	const errorObj = Object.assign({}, defaultErr, err);
-	console.log(errorObj.log);
-	return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware error",
+    status: 400,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
